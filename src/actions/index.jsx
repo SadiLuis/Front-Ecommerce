f0ba71feae674e594edd0a44c7efdd54d31f94e7
Janina Roberto <janinasoledadroberto@gmail.com>
2022-02-04 14:43:28 -0300

    actions:searchByName: in the routes I changed name for title, now it's working

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -7,7 +7,7 @@ import { SEARCH_BY_NAME } from "./types";
 export function searchByName(payload){
         return async function(dispatch){
             try {
-                var json= await axios.get("http://localhost:3001/products?name=" + payload) //OJO: VER BIEN LA ruta por query del back
+                var json= await axios.get("http://localhost:3001/products?title=" + payload) //OJO: VER BIEN LA ruta por query del back
                 return dispatch({
                     type: SEARCH_BY_NAME,
                     payload:json.data
