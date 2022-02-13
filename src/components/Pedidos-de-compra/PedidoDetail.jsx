27ffa9d77a012203a139cb3211ea7aa26b955232
Emi <emibarberis26@gmail.com>
2022-02-12 22:11:56 -0300

    pop up detalle de envio

--- src/components/Pedidos-de-compra/PedidoDetail.jsx
+++ src/components/Pedidos-de-compra/PedidoDetail.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React ,{ useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deletePedido } from "../../actions/index";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
+import { Button , Modal} from "react-bootstrap";
 
 const PedidosCompra = () => {
+  const [show , setShow] = useState(false)
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const pedidoDetail =
     useSelector((state) => state.pedidosReducer.pedidoDetail) || {};
   console.log(pedidoDetail);
   const { totalPedido, status, pagado, productos, pedidoId } = pedidoDetail;
+  const detailSend = useSelector((state) => state.loginReducer.userDetail)
+
 
   const handleBtnCompra = async (e) => {
     e.preventDefault();
-    navigate("/pedido/payment");
+    setShow(true)
+   
   };
 
   const handleBtnCancelar = async (e) => {
@@ -23,6 +27,41 @@ const PedidosCompra = () => {
     dispatch(deletePedido(pedidoId));
     navigate("/home");
   };
+
+
+  const PopUp =(props)=> {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Detalle de envio
+          </Modal.Title>
+        </Modal.Header>
+          <img src={detailSend.avatar} alt="Profile" style={{width:'100px'}}/>
+        <Modal.Body>
+          <h4>Nombre</h4>
+          <p>{detailSend.nombre}</p>
+          <h4>Pais</h4>
+          <p>{detailSend.pais}</p>
+          <h4>Provincia</h4>
+          <p>{detailSend.provincia}</p>
+          <h4>Direccion</h4>
+          <p>{detailSend.direccion}</p>
+          </Modal.Body>
+        <Modal.Footer>
+         
+          <Button  onClick={ ()=> navigate("/pedido/payment")}>Confirmar</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
+  
 
   return status ? (
     <div>
@@ -58,6 +97,10 @@ const PedidosCompra = () => {
       <Button variant="danger" onClick={handleBtnCancelar}>
         Cancelar compra
       </Button>
+      <PopUp
+        show={show}
+        onHide={() => setShow(false)}
+      />
     </div>
   ) : (
     <span>Loading...</span>
